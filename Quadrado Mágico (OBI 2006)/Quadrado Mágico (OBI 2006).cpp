ef98037cd76d87c0d589d764b806f24b1a617825
Glauco Acassio <69947061+glaucoacassio@users.noreply.github.com>
2020-08-23 21:24:34 -0300

    Create Quadrado Mágico (OBI 2006).cpp

---
+++ Quadrado Mágico (OBI 2006)/Quadrado Mágico (OBI 2006).cpp
@@ -0,0 +1,87 @@
+/*
+
+Observacao:
+- Se voce pegar esse mesmo codigo e rodar no site do URI dar resposta errada ou erro de apresentacao(nao sei o porque),
+no site da OBI e do NEPS roda perfeito mas la dar esse problema, talvez seja algum bug ou casos de teste que nao tentei e nem vou. 
+
+*/
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <functional>
+#include <map>
+#include <set>
+#include <string>
+#include <cstring>
+#include <cstdio>
+#include <stack>
+#include <string.h>
+#include <queue>
+#include <iomanip>
+#include <math.h>
+#include <utility>
+#include <sstream>
+#include <cstdlib>
+#include <fstream>
+using namespace std;
+
+int main(int argc, char *argvs[])
+{
+    #ifndef ONLINE_JUDGE
+        freopen("in.txt", "r", stdin);
+        freopen("out.txt", "w", stdout);
+    #endif
+    int a[3][3], soma = 0;
+
+    for (int i = 0; i < 3; ++i)
+        for (int j = 0; j < 3; ++j)
+            cin >> a[i][j];
+
+    for (int i = 0; i < 3; ++i)
+        if ((a[i][0] == 0) + (a[i][1] == 0) + (a[i][2] == 0) == 0)
+            soma = a[i][0] + a[i][1] + a[i][2];
+
+    for (int j = 0; j < 3; ++j)
+        if ((a[0][j] == 0) + (a[1][j] == 0) + (a[2][j] == 0) == 0)
+            soma = a[0][j] + a[1][j] + a[2][j];
+
+    if ((a[0][0] == 0) + (a[1][1] == 0) + (a[2][2] == 0) == 0)
+        soma = a[0][0] + a[1][1] + a[2][2];
+
+    if ((a[0][2] == 0) + (a[1][1] == 0) + (a[2][0] == 0) == 0)
+        soma = a[0][2] + a[1][1] + a[2][0];
+
+    if (soma == 0)
+    {
+        for (int i = 0; i < 3; ++i)
+            for (int j = 0; j < 3; ++j)
+                soma += a[i][j];
+        soma /= 2;
+    }
+
+    for (int tentativas = 0; tentativas < 3; ++tentativas)
+    {
+        for (int i = 0; i < 3; ++i)
+            for (int j = 0; j < 3; ++j)
+            {
+
+                /*Testa mesma linha*/
+                if ((a[i][j] == 0) && (a[i][0] == 0) + (a[i][1] == 0) + (a[i][2] == 0) == 1)
+                    a[i][j] = soma - a[i][0] - a[i][1] - a[i][2];
+
+                /*Testa mesma coluna*/
+                if ((a[i][j] == 0) && (a[0][j] == 0) + (a[1][j] == 0) + (a[2][j] == 0) == 1)
+                    a[i][j] = soma - a[0][j] - a[1][j] - a[2][j];
+            }
+    }
+
+    for (int i = 0; i < 3; ++i)
+    {
+        for (int j = 0; j < 3; ++j)
+            cout << a[i][j] << " ";
+        cout<<endl;
+    }
+
+    return 0;
+}
