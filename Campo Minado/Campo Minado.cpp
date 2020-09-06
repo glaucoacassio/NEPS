eb460201c5eef4d91527a7a1b1aa453d8634ccd9
Glauco Acassio <69947061+glaucoacassio@users.noreply.github.com>
2020-09-05 23:51:52 -0300

    Update Campo Minado.cpp

--- Campo Minado/Campo Minado.cpp
+++ Campo Minado/Campo Minado.cpp
@@ -23,7 +23,7 @@ int main()
 
     scanf("%d",&N);
     vector<int> campo(N);
-    for (int i =0; i<N; i++)
+    for (int i = 0; i < N; i++)
         scanf("%d",&campo[i]);
     
     for (int i = 0; i < N; i++)
